6df82dc83d04f8cf72178024e0f9173a86707f2e
luibasantes <luibasantes@hotmail.com>
2017-10-21 03:36:52 -0500

    Layout cargando, Layout menu principal

---
+++ app/src/main/java/koukin/llapanmi/Activity_Intro.java
@@ -0,0 +1,53 @@
+package koukin.llapanmi;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.WindowManager;
+
+public class Activity_Intro extends AppCompatActivity {
+
+    private int tiempo = 10;
+    int pStatus = 0;
+    private Handler handler = new Handler();
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        Thread thread = new Thread() {
+            public void run() {
+                while (pStatus < 300) {
+                    pStatus += 1;
+                    handler.post(new Runnable() {
+                        @Override
+                        public void run() {
+
+                        }
+                    });
+                    try {
+                        sleep(tiempo);
+                    } catch (Exception e) {
+
+                    }
+                }
+
+                Intent i=new Intent(getBaseContext(),Activity_Menu_Principal.class);
+                startActivity(i);
+
+
+                finish();
+
+
+
+
+            }
+        };
+        thread.start();
+
+    }
+}
---
+++ app/src/main/java/koukin/llapanmi/Activity_Menu_Principal.java
@@ -0,0 +1,18 @@
+package koukin.llapanmi;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Luigi on 21/10/2017.
+ */
+
+public class Activity_Menu_Principal extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_principal);
+
+
+    }
+}
