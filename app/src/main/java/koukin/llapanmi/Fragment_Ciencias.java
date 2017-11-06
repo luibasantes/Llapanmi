87aac70e91bded33e9eb77afd27ffafc95d41f35
luibasantes <luibasantes@hotmail.com>
2017-11-06 01:22:25 -0500

    Agregando fragments

---
+++ app/src/main/java/koukin/llapanmi/Fragment_Ciencias.java
@@ -0,0 +1,23 @@
+package koukin.llapanmi;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Luigi on 06/11/2017.
+ */
+
+public class Fragment_Ciencias extends android.support.v4.app.Fragment{
+    private static final String TAG ="Fragment2";
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_preguntas_ciencia_texto,container,false);
+        return view;
+    }
+}
---
+++ app/src/main/java/koukin/llapanmi/Fragment_Matematicas.java
@@ -0,0 +1,23 @@
+package koukin.llapanmi;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Luigi on 06/11/2017.
+ */
+
+public class Fragment_Matematicas extends android.support.v4.app.Fragment{
+    private static final String TAG ="Fragment4";
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_preguntas_matematicas_texto,container,false);
+        return view;
+    }
+}
